03a7bbca457e4fa80fcdd674d534df097a696e40
Mokey-D-luffy88 <atching123@gmail.com>
2020-03-30 17:36:18 +0530

    this function is to add data first in data and then add only pre-success data in queue.

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,43 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
-// executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        allQueueFunction();
+    }
+
+    private static void allQueueFunction() {
+        Scanner tell = new Scanner(System.in);
+        System.out.println("your are going add data in tree structure and printing out all the pre - order successor");
+        boolean exit = false;
+        int atLestInput = 3;
+        System.out.println("provide data to tree ablest 3 input are required to outcome with better out put :");
+        MyBinarySearchTree<Integer> binaryAccess = new MyBinarySearchTree<>();
+        while (exit != true) {
+            do {
+                binaryAccess.add(tell.nextInt());
+                atLestInput--;
+            } while (atLestInput > 0);
+            System.out.print("do you like add more (y/n) : ");
+            char nm = (tell.next()).charAt(0);
+            if (nm == 'n') {
+                exit = true;
+            }
+        }
+        perOrderSuccess(binaryAccess);
+        binaryAccess.traverse();
+    }
+
+    private static void perOrderSuccess(MyBinarySearchTree<Integer> intoQueue) {
+
+        for (int i = 0; i < intoQueue.getSize(); i++) {
+
+        }
+    }
+
 }
