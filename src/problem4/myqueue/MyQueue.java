fcf398f11f2d7650947e218ac58dc49798bf2f0b
Mokey-D-luffy88 <atching123@gmail.com>
2020-03-30 15:57:43 +0530

    this function will add node in queue

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,5 +7,28 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
-public class MyQueue {
+import problem3.node.Node;
+
+public class MyQueue<E> {
+    private Node<E> front = null;
+    private Node<E> rear = null;
+    private int size = 0;
+
+    public void add(E data) {
+        front = addInQueue(data);
+        size++;
+    }
+
+    private Node<E> addInQueue(E data) {
+        if (front == null) {
+            rear = front = new Node<>(data);
+        } else {
+            Node<E> temp = new Node<>(data);
+            rear.setNext(temp);
+            front = rear;
+            rear = temp;
+        }
+        return front;
+    }
+
 }
