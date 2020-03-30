5187bde5472223f06a7008f7e1a8d9165e90273f
Mokey-D-luffy88 <atching123@gmail.com>
2020-03-30 12:16:25 +0530

    messages have been given .

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -5,6 +5,7 @@
  *  Time: 7:17 PM
  */
 package problem1.main;
+
 import problem1.mybst.MyBinarySearchTree;
 
 import java.util.Scanner;
@@ -17,8 +18,9 @@ public class MyMain {
     public static void menuFunction() {
         boolean turnOff = false;
         Scanner tell = new Scanner(System.in);
+        MyBinarySearchTree<Integer> familyMember = new MyBinarySearchTree();
         while (turnOff != true) {
-            System.out.println("Enter the choice from Menu : \n" +
+            System.out.println("\nEnter the choice from Menu : \n" +
                     "1.Add a new record\n" +
                     "2.Print record's\n" +
                     "3.Delete record's\n" +
@@ -29,17 +31,22 @@ public class MyMain {
              * enhanced switch is been introduce in java 13 or later
              * enhanced switch does not required break keyword.
              */
-            MyBinarySearchTree<Integer> familyMember = new MyBinarySearchTree();
             switch (tell.nextInt()) {
                 case 1 -> {
-                    System.out.println("Enter the No :");
+                    System.out.println("\nEnter the number : ");
                     familyMember.add(tell.nextInt());
                 }
-                case 2 -> familyMember.traverse();
-                case 3 -> familyMember.remove(tell.nextInt());
+                case 2 -> {
+                    System.out.println("\nleft Member of family :");
+                    familyMember.traverse();
+                }
+                case 3 -> {
+                    System.out.println("\nEnter the Number : ");
+                    familyMember.remove(tell.nextInt());
+                }
                 case 4 -> familyMember.search(tell.nextInt());
                 case 5 -> turnOff = true;
-                default -> throw new IllegalStateException("Invalid Input:");
+                default -> throw new IllegalStateException("\nInvalid Input:");
             }
         }
     }
