5875860c7543cd7e2367ed9711864b644a671b8c
Mokey-D-luffy88 <atching123@gmail.com>
2020-03-30 12:48:59 +0530

    search data is been found recursively and message is print in main method.

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -21,11 +21,12 @@ public class MyMain {
         MyBinarySearchTree<Integer> familyMember = new MyBinarySearchTree();
         while (turnOff != true) {
             System.out.println("\nEnter the choice from Menu : \n" +
-                    "1.Add a new record\n" +
+                    "\n1.Add a new record\n" +
                     "2.Print record's\n" +
                     "3.Delete record's\n" +
                     "4.Search record's\n" +
                     "5.Exit\n");
+            System.out.print("Enter the choice : ");
             /**
              * here I have used enhanced switch case the menu selection
              * enhanced switch is been introduce in java 13 or later
@@ -33,18 +34,26 @@ public class MyMain {
              */
             switch (tell.nextInt()) {
                 case 1 -> {
-                    System.out.println("\nEnter the number : ");
+                    System.out.print("\nAdd new member : ");
                     familyMember.add(tell.nextInt());
                 }
                 case 2 -> {
-                    System.out.println("\nleft Member of family :");
+                    System.out.print("\nleft Member of family : ");
                     familyMember.traverse();
                 }
                 case 3 -> {
-                    System.out.println("\nEnter the Number : ");
+                    System.out.print("\nEnter the Number : ");
                     familyMember.remove(tell.nextInt());
                 }
-                case 4 -> familyMember.search(tell.nextInt());
+                case 4 -> {
+                    System.out.print("\nEnter the number to search For : ");
+                    boolean found = familyMember.search(tell.nextInt());
+                    if (found) {
+                        System.out.println("Sorry member have not found");
+                    } else {
+                        System.out.println("Your member have been found");
+                    }
+                }
                 case 5 -> turnOff = true;
                 default -> throw new IllegalStateException("\nInvalid Input:");
             }
